19bdfd36cdaa41bf48636279cf4cc4a3514a32ad
cexossqu <cexossqu@163.com>
2022-10-17 23:31:22 +0800

    neovim 配置自动补全已完成

---
+++ init.lua
@@ -0,0 +1,31 @@
+require('basic')
+
+require("keybindings")
+
+-- Packer 插件管理
+require("plugins")
+
+-- 主题设置 （新增）
+require("colorscheme")
+
+require("plugin-config.nvim-tree")
+
+require("plugin-config.bufferline")
+
+require("plugin-config.lualine")
+
+require("plugin-config.telescope")
+
+require("plugin-config.dashboard")
+
+require("plugin-config.project")
+
+require("plugin-config.nvim-treesitter")
+
+require("lsp.setup")
+
+require("lsp.cmp")
+
+require("lsp.ui")
+
+require("lsp.null-ls")
